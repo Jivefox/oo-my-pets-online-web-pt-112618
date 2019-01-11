1d9a87bfdc7e77d6d3ec3f33047b3f2e24857826
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-11 04:28:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -31,11 +31,11 @@ class Owner
     @pets[:fishes] << Fish.new(fish)
   end
   
-  def buy_cat
-    
+  def buy_cat(cat)
+    @pets[:cats] << Cat.new(cat)
   end
   
   def buy_dog
-    
+    @pets[:dogs] << Dog.new(dog)
   end
 end
